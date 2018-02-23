537ea98071950bf51fbaf5dae95f9ad68474dedc
brad ferrell <ferrellbh@gmail.com>
2018-02-22 22:13:08 -0500

    updated comments

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 // Setting variables for the game
 
-//Array of letters that the computer can choose from
+//Array of letters that the computer can choose from.
 var alphabet = [
   "a",
   "b",
@@ -30,31 +30,35 @@ var alphabet = [
   "z"
 ];
 
-//User's wins, losses and number of guesses remaining in the game
+//User's wins, losses and number of guesses remaining in the game.
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 
-//compGuess variable is the result of the computer randomly picking a letter from above array
+//compGuess variable is the result of the computer randomly picking a letter from above array.
 var compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 
 //Listening for events...
 document.onkeyup = function(event) {
   var userGuess = event.key.toLowerCase();
 
+  //If the user's guess is equal to the computer's guess, add 1 win and reduce guesses by 1.
   if (userGuess === compGuess) {
     wins++, guessesLeft--;
   } else {
+    //Else, reduce guesses by 1 and re-run the random letter selection process.
     guessesLeft--,
       (compGuess = alphabet[Math.floor(Math.random() * alphabet.length)]);
   }
 
+  //If the total number of guesses reaches 0, add 1 loss, reset total guesses to 9, and re-run the random letter selection process to begin a new round of 9 guesses.
   if (guessesLeft === 0) {
     losses++,
       (guessesLeft = 9),
       (compGuess = alphabet[Math.floor(Math.random() * alphabet.length)]);
   }
 
+  //Add inner HTML to page.
   var html =
     "<p>You guessed: " +
     userGuess +
