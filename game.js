3024a2e8da66e325907c00fa02672fe1da983940
brad ferrell <ferrellbh@gmail.com>
2018-02-24 17:52:45 -0500

    game update

--- game.js
+++ game.js
@@ -1,7 +1,4 @@
-// Setting variables for the game
-
-//Array of letters that the computer can choose from.
-var alphabet = [
+var compChoices = [
   "a",
   "b",
   "c",
@@ -30,52 +27,49 @@ var alphabet = [
   "z"
 ];
 
-//User's wins, losses and number of guesses remaining in the game.
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
+var guessChoices = [];
 
-//compGuess variable is the result of the computer randomly picking a letter from above array.
-var compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-
-//Listening for events...
 document.onkeyup = function(event) {
-  var userGuess = event.key.toLowerCase();
+  var userGuess = event.key;
 
-  //If the user's guess is equal to the computer's guess, add 1 win, re-run random letter selection process and reset guesses to 9.
+  var compGuess = compChoices[Math.floor(Math.random() * compChoices.length)];
+  console.log(compGuess);
+
   if (userGuess === compGuess) {
-    wins++,
-      (guessesLeft = 9),
-      (compGuess = alphabet[Math.floor(Math.random() * alphabet.length)]);
-  } else {
-    //Else, reduce guesses by 1 and re-run the random letter selection process.
-    guessesLeft--,
-      (compGuess = alphabet[Math.floor(Math.random() * alphabet.length)]);
-  }
-  //If the total number of guesses reaches 0, add 1 loss, re-run random letter selection process and reset guesses to 9.
-  if (guessesLeft < 1) {
-    losses++,
-      (guessesLeft = 9),
-      (compGuess = alphabet[Math.floor(Math.random() * alphabet.length)]);
+    wins++;
+    numGuesses = 9;
+    guessChoices = [];
   }
 
-  //Add inner HTML to page.
+  if (userGuess != compGuess) {
+    guessesLeft--;
+    guessChoices.push(userGuess);
+  }
+
+  if (guessesLeft === 0) {
+    guessesLeft = 9;
+    losses++;
+    guessChoices = [];
+  }
+
   var html =
-    "<p>You guessed: " +
-    userGuess +
-    "</p>" +
-    "<p>Computer guessed: " +
-    compGuess +
-    "</p>" +
-    "<p>Guesses Left: " +
-    guessesLeft +
+    "<h1> The Psychic Game </h1>" +
+    "<p>Guess what letter I'm thinking of!</p>" +
+    "<p>Your Guesses so far: " +
+    guessChoices.join(", ") +
     "</p>" +
     "<p>Wins: " +
     wins +
     "</p>" +
     "<p>Losses: " +
     losses +
+    "</p>" +
+    "<p>Guesses Left: " +
+    guessesLeft +
     "</p>";
 
-  document.getElementById("game").innerHTML = html;
+  document.querySelector("#game").innerHTML = html;
 };
